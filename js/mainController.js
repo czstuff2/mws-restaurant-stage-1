59d768898891604d14c07c78d94dd0da873a2369
Scott <getsomegr@gmail.com>
2018-05-29 16:26:33 -0400

    Implementing Sw

---
+++ js/mainController.js
@@ -0,0 +1,11 @@
+const registerServiceWorker = function() {
+	if (!navigator.serviceWorker) return;
+
+	navigator.serviceWorker.register('js/sw.js').then(function() {
+		console.log('Registration worked!');
+	}).catch(function() {
+		console.log('Registration failed!');
+	});
+};
+
+registerServiceWorker();
