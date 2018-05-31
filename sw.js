45e1e42b943decaddcce43220a70d8a9e4dd53c9
Scott <getsomegr@gmail.com>
2018-05-31 16:21:35 -0400

    Cached all restaurant id's
    added all 10 urls

--- sw.js
+++ sw.js
@@ -6,10 +6,21 @@ self.addEventListener('install', function(event) {
 		'js/dbhelper.js',
 		'js/main.js',
 		'js/mainController.js',
+		'js/restaurant_info.js',
 		'data/restaurants.json',
 		'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
 		'https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxK.woff2',
-		'https://fonts.gstatic.com/s/roboto/v18/KFOlCnqEu92Fr1MmEU9fBBc4.woff2'
+		'https://fonts.gstatic.com/s/roboto/v18/KFOlCnqEu92Fr1MmEU9fBBc4.woff2',
+		'restaurant.html?id=1',
+		'restaurant.html?id=2',
+		'restaurant.html?id=3',
+		'restaurant.html?id=4',
+		'restaurant.html?id=5',
+		'restaurant.html?id=6',
+		'restaurant.html?id=7',
+		'restaurant.html?id=8',
+		'restaurant.html?id=9',
+		'restaurant.html?id=10' 
 	];
 
 	event.waitUntil(
@@ -23,7 +34,6 @@ self.addEventListener('fetch', function(event) {
 	let urlsToCache = [];
 	event.respondWith(
 		caches.match(event.request).then(function(response) {
-			console.log(url.pathname);
 			/* If a cached asset is found, return it */
 			if (response) {
 				return response;
@@ -32,7 +42,7 @@ self.addEventListener('fetch', function(event) {
 				urlsToCache.push(event.request.url);
 				caches.open('mytest').then(function(cache) {
 					cache.addAll(urlsToCache);
-				})
+				})	
 			}
 			/* If uncached asset is not an img, fetch it normally and return it */
 			return fetch(event.request);
