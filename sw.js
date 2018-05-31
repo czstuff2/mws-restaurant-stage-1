79f40a603381936a555b95b4b7b57e4ef87fd76a
Scott <getsomegr@gmail.com>
2018-05-31 15:26:44 -0400

    added log to events
    fixed port 8000

--- sw.js
+++ sw.js
@@ -23,19 +23,18 @@ self.addEventListener('fetch', function(event) {
 	let urlsToCache = [];
 	event.respondWith(
 		caches.match(event.request).then(function(response) {
+			console.log(url.pathname);
 			/* If a cached asset is found, return it */
 			if (response) {
 				return response;
 			/* If the uncached asset is an img cache it for next time */
 			} else if (event.request.url.startsWith("http://localhost:3000/img")) {
 				urlsToCache.push(event.request.url);
-				console.log("Cached an Image");
 				caches.open('mytest').then(function(cache) {
 					cache.addAll(urlsToCache);
 				})
 			}
 			/* If uncached asset is not an img, fetch it normally and return it */
-			console.log("NetWork Pull");
 			return fetch(event.request);
 		})
 	);
