8f7d3337b2576be7cc66f0d9b71798c3ecb3f11a
Scott <getsomegr@gmail.com>
2018-05-31 11:15:01 -0400

    sw install event
    added array of known files on Install

--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
-self.addEventListener('fetch', function() {
+self.addEventListener('fetch', function(event) {
 	console.log(event.request);
+});
+
+
+self.addEventListener('install', function(event) {
+	const urlsToCache = [
+		'/',
+		'css/styles.css',
+		'css/responsive.css',
+		'js/dbhelper.js',
+		'js/main.js',
+		'js/mainController.js',
+		'data/restaurants.json',
+		'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
+		'https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxK.woff2',
+		'https://fonts.gstatic.com/s/roboto/v18/KFOlCnqEu92Fr1MmEU9fBBc4.woff2'
+
+	];
+	caches.open('mytest').then(function(cache) {
+
+	});
 });
