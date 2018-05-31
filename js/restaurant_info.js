7192bc89a45e57b92391b68bfad24d4ef98d07c8
Scott <getsomegr@gmail.com>
2018-05-31 13:13:44 -0400

    Fix alt attribute
    fixed alt attribute in json
    added property into main and restaurant info

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -58,7 +58,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.setAttribute("alt", restaurant.name);
+  image.setAttribute("alt", restaurant.name + ".  " + restaurant.alt);
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
