92b33b66310343c74a6f58e343f4ac01f514bae6
Scott <getsomegr@gmail.com>
2018-05-31 12:25:44 -0400

    Commented SW fetch event
    returns any cached assets, adds any imgs and if neither fetches

--- sw.js
+++ sw.js
@@ -20,12 +20,22 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+	let urlsToCache = [];
 	event.respondWith(
 		caches.match(event.request).then(function(response) {
+			/* If a cached asset is found, return it */
 			if (response) {
-				console.log("Found something in the cache.")
 				return response;
-			};
+			/* If the uncached asset is an img cache it for next time */
+			} else if (event.request.url.startsWith("http://localhost:3000/img")) {
+				urlsToCache.push(event.request.url);
+				console.log("Cached an Image");
+				caches.open('mytest').then(function(cache) {
+					cache.addAll(urlsToCache);
+				})
+			}
+			/* If uncached asset is not an img, fetch it normally and return it */
+			console.log("NetWork Pull");
 			return fetch(event.request);
 		})
 	);
