e9d3645e4a75796ef93dd77bccf7abe1614928c5
Scott <getsomegr@gmail.com>
2018-05-31 10:28:36 -0400

    Added role application to maps

--- js/mainController.js
+++ js/mainController.js
@@ -1,7 +1,7 @@
 const registerServiceWorker = function() {
 	if (!navigator.serviceWorker) return;
 
-	navigator.serviceWorker.register('js/sw.js').then(function() {
+	navigator.serviceWorker.register('sw.js').then(function() {
 		console.log('Registration worked!');
 	}).catch(function() {
 		console.log('Registration failed!');
